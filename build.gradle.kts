41a728f50e1c19c1268e1bcb232cd7adefb3fdea
Hamiz Ghani <hamizghaniiii@gmail.com>
2026-02-10 16:18:39 +0700

    fix: some depedencies update and remove mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,19 +11,10 @@ description = "eshop"
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
-
-
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
     }
 }
 
@@ -33,40 +24,32 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
     compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
-    testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
-    testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitJupiterVersion}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitJupiterVersion}")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
 }
+
 tasks.register<Test>("unitTest") {
     description = "Runs unit tests."
     group = "verification"
-
-    filter {
-        excludeTestsMatching("*FunctionalTest")
-    }
+    filter { excludeTestsMatching("*FunctionalTest") }
 }
 
 tasks.register<Test>("functionalTest") {
     description = "Runs functional tests."
     group = "verification"
-
-    filter {
-        includeTestsMatching("*FunctionalTest")
-    }
+    filter { includeTestsMatching("*FunctionalTest") }
 }
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
-
