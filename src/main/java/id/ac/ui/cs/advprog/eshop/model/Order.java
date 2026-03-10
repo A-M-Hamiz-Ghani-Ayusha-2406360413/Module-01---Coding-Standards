8b967afbbf36e8ca76a4ae037392c479af7df671
Hamiz Ghani <hamizghaniiii@gmail.com>
2026-03-10 15:38:42 +0700

    [GREEN] Implement Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,24 +1,45 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
-@Builder
 @Getter
 public class Order {
     String id;
     List<Product> products;
     Long orderTime;
     String author;
-    @Setter
     String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author) {
+        this.id = id;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = "WAITING_PAYMENT";
+
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.products = products;
+        }
     }
 
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+        this(id, products, orderTime, author);
+        this.setStatus(status);
+    }
+
+    public void setStatus(String status) {
+        if (isValidStatus(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isValidStatus(String status) {
+        return status.equals("WAITING_PAYMENT") || status.equals("FAILED") ||
+               status.equals("SUCCESS") || status.equals("CANCELLED");
     }
 }
